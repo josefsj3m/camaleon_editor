fb9617ee20e7291a4a6d4d0c295c0f4ab665c433
Jose Fernando <josefsmvm@gmail.com>
2021-01-15 13:22:15 -0300

    Primeiro commit

---
+++ app/assets/javascripts/plugins/camaleon_editor/admin/editor-manifest.js
@@ -0,0 +1,13 @@
+//= require camaleon_cms/admin/bootstrap-colorpicker
+//= require ./accordion
+//= require ./audio
+//= require ./gallery
+//= require ./image
+//= require ./slider
+//= require ./tabs
+//= require ./text
+//= require ./video
+//= require ./editor
+
+//= require ./grid-editor
+//= require ./grid_editor_style
