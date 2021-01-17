7f7f1ce9434e53bd821bce779dd5c7b0ff7c97da
Jose Fernando <josefsmvm@gmail.com>
2021-01-17 14:30:18 -0300

    Fized bugs

--- app/assets/javascripts/plugins/camaleon_editor/admin/grid-editor.js
+++ app/assets/javascripts/plugins/camaleon_editor/admin/grid-editor.js
@@ -8,14 +8,14 @@ jQuery(function(){
     $.fn.gridEditor_libraries = [];
     //********************** editor content options **********************//
     $.fn.gridEditor_options = {
-        text: {title: "Text", description: I18n.t("mvm_js.allows_plain_text", {defaultValue: "Allows you to include plain text in any column."}), libraries: [], callback: grid_text_builder},
-        editor: {title: "Editor", description: I18n.t("mvm_js.allows_html_text", {defaultValue: "Allows you to include html text in any column."}), libraries: [], callback: grid_editor_builder},
-        tab: {title: "Tabs", description: I18n.t("mvm_js.allows_tabs_container", {defaultValue: "Allows you to include tabs container in any column."}), callback: grid_tab_builder},
-        slider: {title: "Slider", description: I18n.t("mvm_js.allows_slider_animation", {defaultValue: "Allows you to include a slider animation in any column.",}), callback: grid_slider_builder},
-        image: {title: "Image", description: I18n.t("mvm_js.allows_image", {defaultValue: "Allows you to include an image."}), callback: grid_image_builder},
-        video: {title: "Video", description: I18n.t("mvm_js.allows_video", {defaultValue: "Allows you to include a video."}), callback: grid_video_builder},
-        audio: {title: "Audio", description: I18n.t("mvm_js.allows_audio", {defaultValue: "Allows you to include a audio."}), callback: grid_audio_builder},
-        accordion: {title: "Accordion", description: I18n.t("mvm_js.allows_accordion", {defaultValue: "Allows you to include an accordion in any column."}), callback: grid_accordion_builder},
+        text: {title: "Text", description: I18n("mvm_js.allows_plain_text", {defaultValue: "Allows you to include plain text in any column."}), libraries: [], callback: grid_text_builder},
+        editor: {title: "Editor", description: I18n("mvm_js.allows_html_text", {defaultValue: "Allows you to include html text in any column."}), libraries: [], callback: grid_editor_builder},
+        tab: {title: "Tabs", description: I18n("mvm_js.allows_tabs_container", {defaultValue: "Allows you to include tabs container in any column."}), callback: grid_tab_builder},
+        slider: {title: "Slider", description: I18n("mvm_js.allows_slider_animation", {defaultValue: "Allows you to include a slider animation in any column.",}), callback: grid_slider_builder},
+        image: {title: "Image", description: I18n("mvm_js.allows_image", {defaultValue: "Allows you to include an image."}), callback: grid_image_builder},
+        video: {title: "Video", description: I18n("mvm_js.allows_video", {defaultValue: "Allows you to include a video."}), callback: grid_video_builder},
+        audio: {title: "Audio", description: I18n("mvm_js.allows_audio", {defaultValue: "Allows you to include a audio."}), callback: grid_audio_builder},
+        accordion: {title: "Accordion", description: I18n("mvm_js.allows_accordion", {defaultValue: "Allows you to include an accordion in any column."}), callback: grid_accordion_builder},
         //gallery: {title: "Gallery", description: "Permit you to include a gallery of audio, video or image in any column.", callback: grid_gallery_builder},
     };
     //********************** end editor content options **********************//
@@ -29,10 +29,10 @@ jQuery(function(){
         var textarea = $(this);
         if(textarea.prev().hasClass("panel_grid_editor")){ textarea.prev().show(); return textarea; }
         var tpl_rows = "";
-        $.each({6: 50, 4: 33, 3: 25, 2: 16, 8: 66, 9: 75, 12: 100}, function(k, val){ tpl_rows += '<div class="" data-col="'+k+'" title="'+I18n.t("mvm_js.insert_column_block", {defaultValue: "Insert a column block with"})+'" "'+val+I18n.t("mvm_js.of_width", {defaultValue: "% of width."})+'" data-col_title="'+val+'%"><div class="grid_sortable_items"></div></div>'; });
+        $.each({6: 50, 4: 33, 3: 25, 2: 16, 8: 66, 9: 75, 12: 100}, function(k, val){ tpl_rows += '<div class="" data-col="'+k+'" title="'+I18n("mvm_js.insert_column_block", {defaultValue: "Insert a column block with"})+'" "'+val+I18n("mvm_js.of_width", {defaultValue: "% of width."})+'" data-col_title="'+val+'%"><div class="grid_sortable_items"></div></div>'; });
 
         // break line
-        tpl_rows += '<div class="clearfix" title="'+I18n.t("mvm_js.insert_break_line", {defaultValue: "Insert a break line to sort the column blocks."})+'" data-col_title="'+I18n.t("mvm_js.break_line", {defaultValue: "Break Line"})+'" data-col="12"></div>' + $.fn.gridEditor_extra_rows.join("");
+        tpl_rows += '<div class="clearfix" title="'+I18n("mvm_js.insert_break_line", {defaultValue: "Insert a break line to sort the column blocks."})+'" data-col_title="'+I18n("mvm_js.break_line", {defaultValue: "Break Line"})+'" data-col="12"></div>' + $.fn.gridEditor_extra_rows.join("");
 
         // tpl options
         var tpl_options = "";
