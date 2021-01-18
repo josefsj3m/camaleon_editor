6ee53280453747221e1bcaa404a43b7a39ef8c6c
Jose Fernando <josefsmvm@gmail.com>
2021-01-18 09:41:15 -0300

    Fixed issue with i18n-js

--- app/assets/javascripts/plugins/camaleon_editor/admin/editor-manifest.js
+++ app/assets/javascripts/plugins/camaleon_editor/admin/editor-manifest.js
@@ -11,3 +11,11 @@
 
 //= require ./grid-editor
 //= require ./grid_editor_style
+// This is optional (in case you have `I18n is not defined` error)
+// If you want to put this line, you must put it BEFORE `i18n/translations`
+//= require ./i18n
+// Some people even need to add the extension to make it work, see https://github.com/fnando/i18n-js/issues/283
+// require i18n.js
+//
+// This is a must
+//= require ./translations
--- lib/camaleon_editor/version.rb
+++ lib/camaleon_editor/version.rb
@@ -1,3 +1,3 @@
 module CamaleonEditor
-  VERSION = "0.0.6"
+  VERSION = "0.0.7"
 end
